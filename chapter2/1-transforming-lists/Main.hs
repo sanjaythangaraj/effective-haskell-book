fa999d4752ddc5086a7cfbd2a885be74ed942415
sanjaythangaraj <sanjaythangaraj@outlook.in>
2025-08-06 19:47:16 +0530

    filter map and fold - function composition

--- chapter2/1-transforming-lists/Main.hs
+++ chapter2/1-transforming-lists/Main.hs
@@ -1,9 +1,11 @@
 module Main where
+
 -- import Prelude hiding (foldl, foldr)
 
 doubleElems :: [Int] -> [Int]
 doubleElems nums =
-    if null nums then []
+  if null nums
+    then []
     else (2 * (head nums)) : doubleElems (tail nums)
 
 -- foldr func carryValue lst =
@@ -12,21 +14,34 @@ doubleElems nums =
 --     else func (head lst) $ foldr func carryValue (tail lst)
 
 doubleElems' elems = foldr doubleElem [] elems
-    where
-        doubleElem num lst = (2 * num) : lst
+  where
+    doubleElem num lst = (2 * num) : lst
 
-doubleElems'' elems = foldr (applyElem (*2)) [] elems
-    where
-        applyElem f elem accumulator = f elem : accumulator
+doubleElems'' elems = foldr (applyElem (* 2)) [] elems
+  where
+    applyElem f elem accumulator = f elem : accumulator
 
 map' f = foldr (applyElem f) []
-    where
-        applyElem f elem accumulator = f elem : accumulator
+  where
+    applyElem f elem accumulator = f elem : accumulator
 
-doubleWithMap elems = map' (*2) elems
+doubleWithMap elems = map' (* 2) elems
 
 map'' f xs =
-    if null xs then []
+  if null xs
+    then []
     else f (head xs) : map'' f (tail xs)
+
+-- filtering list elements
+
+checkGuestList guestList name =
+  name `elem` guestList
+
+foodCosts = [("Ren", 10.00), ("George", 4.00), ("Porter", 27.50)]
+
+partyBudget isAttending =
+    foldr (+) 0 . map snd . filter (isAttending . fst)
+
+-- partyBudget (checkGuestList ["Ren", "Porter"]) foodCosts
 
 main = print "Hello World"
